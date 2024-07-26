2a623636bbaedb31411097028aa011756307684a
parkjunhee321 <jhee.park@moyoplan.com>
2024-07-27 00:51:02 +0900

    feat: 아티클 목록 컴포넌트로 분리

---
+++ components/contents.js
@@ -0,0 +1,10 @@
+export default function Contents() {
+  return `<div class="banner-image-container">
+  <img class="banner-image" src="./assets/tosstect_banner.webp" alt="배너이미지">
+</div>
+<section class="contents-list-wrapper">
+  <ul class="contents-list">
+  
+  </ul>
+</section>`;
+}
